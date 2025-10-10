22f6c0ecc436e63e4a9e2e01c9908ba5400ffea4
Jack <jackrieck@users.noreply.github.com>
2025-10-10 15:12:47 -0400

    Cv1 (#4)
    * update idl
    
    * try adding seeds
    
    * add all seeds
    
    * rename module

--- clend-protocol-cpi/src/lib.rs
+++ clend-protocol-cpi/src/lib.rs
@@ -1,3 +1,5 @@
 anchor_gen::generate_cpi_interface!(idl_path = "idl.json");
 
 declare_id!("C73nDAFn23RYwiFa6vtHshSbcg8x6BLYjw3bERJ3vHxf");
+
+pub mod seeds;
---
+++ clend-protocol-cpi/src/seeds.rs
@@ -0,0 +1,17 @@
+pub const BANK_LIQUIDITY_VAULT_SEED: &'static [u8] = b"liquidity_vault";
+
+pub const BANK_LIQUIDITY_VAULT_AUTHORITY_SEED: &'static [u8] = b"liquidity_vault_auth";
+
+pub const BANK_INSURANCE_VAULT_SEED: &'static [u8] = b"insurance_vault";
+
+pub const BANK_INSURANCE_VAULT_AUTHORITY_SEED: &'static [u8] = b"insurance_vault_auth";
+
+pub const BANK_FEE_VAULT_SEED: &'static [u8] = b"fee_vault";
+
+pub const BANK_FEE_VAULT_AUTHORITY_SEED: &'static [u8] = b"fee_vault_auth";
+
+pub const FEE_STATE_SEED: &'static [u8] = b"fee_state";
+
+pub const EMISSIONS_AUTHORITY_SEED: &'static [u8] = b"emissions_auth_seed";
+
+pub const EMISSIONS_TOKEN_ACCOUNT_SEED: &'static [u8] = b"emissions_token_account_seed";
