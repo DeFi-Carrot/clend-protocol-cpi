f9e9e3ca8e7c13204bc4c0d231acf41d2aea7525
John Rieck <jackdrieck@gmail.com>
2025-12-05 12:36:25 -0500

    add func instead of str hardcoded

--- clend-protocol-cpi/src/addresses.rs
+++ clend-protocol-cpi/src/addresses.rs
@@ -1,1 +1,7 @@
-pub const EVENT_AUTHORITY_ADDRESS: &str = "CVq8mEtBtcc74sx4zgjtS9AHvcsL8DsKFugU55QeK7jd";
+use std::str::FromStr;
+
+use anchor_lang::prelude::Pubkey;
+
+pub fn event_authority_address() -> Pubkey {
+    Pubkey::from_str("CVq8mEtBtcc74sx4zgjtS9AHvcsL8DsKFugU55QeK7jd").unwrap()
+}
