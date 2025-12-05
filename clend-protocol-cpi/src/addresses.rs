323c23a6aa63070765e967e83d9d7b40c2a4d1c0
John Rieck <jackdrieck@gmail.com>
2025-12-05 12:38:39 -0500

    comments

--- clend-protocol-cpi/src/addresses.rs
+++ clend-protocol-cpi/src/addresses.rs
@@ -2,6 +2,9 @@ use std::str::FromStr;
 
 use anchor_lang::prelude::Pubkey;
 
-pub fn clend_event_authority() -> Pubkey {
+// clend event authority address
+// this is the same for the entire clend protocol
+// used by clend protocol to emit events via CPI
+pub fn clend_ea() -> Pubkey {
     Pubkey::from_str("CVq8mEtBtcc74sx4zgjtS9AHvcsL8DsKFugU55QeK7jd").unwrap()
 }
