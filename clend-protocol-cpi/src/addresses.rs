9d39f8491d55128c16bbacbd9ac2ed4d89427285
John Rieck <jackdrieck@gmail.com>
2025-12-05 12:37:51 -0500

    fix name of fun

--- clend-protocol-cpi/src/addresses.rs
+++ clend-protocol-cpi/src/addresses.rs
@@ -2,6 +2,6 @@ use std::str::FromStr;
 
 use anchor_lang::prelude::Pubkey;
 
-pub fn event_authority_address() -> Pubkey {
+pub fn clend_event_authority() -> Pubkey {
     Pubkey::from_str("CVq8mEtBtcc74sx4zgjtS9AHvcsL8DsKFugU55QeK7jd").unwrap()
 }
